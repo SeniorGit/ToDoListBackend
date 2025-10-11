5a49174f9804e02ff5798544cfcc34125481f9c8
Alfito Nur Fadhila <88030166+SeniorGit@users.noreply.github.com>
2025-10-11 21:15:56 +0700

    Update accesing frontend

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors()); 
+app.use(cors({origin:['https://to-do-list-rho-ashen.vercel.app/']})); 
 app.use(express.json()); 
 
 // Routes
 app.use('/tasks', taskRoutes);
 app.use('/api/auth',  authRouter);
 
-const PORT =  3000;
-app.listen(PORT, () => {
+const PORT =  process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
